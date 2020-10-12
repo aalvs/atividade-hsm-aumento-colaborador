90498e7489c4e7359ea48c37a5c84d525cdab7d1
Andre Alves <andrealvesdeoliveira@outlook.com>
2020-10-12 00:29:39 -0300

    Finalizado atividade 04 do modulo de JavaScript do curso HSM University

---
+++ ColaboradoresAumento/script.js
@@ -0,0 +1,33 @@
+
+function update() {
+
+    let salario = document.querySelector('#input-salario').value;
+    let salarioFormatado =  parseFloat(salario);
+
+    let plano = document.querySelector('#tipo-plano');
+    let itemSelect = plano.options[plano.selectedIndex].value;
+    
+    let aumentoSalario = 0;
+    let novoSalario = document.querySelector('#novoSalario');
+
+    switch(itemSelect) {
+        case 'A':
+            aumentoSalario = salarioFormatado + (salarioFormatado * 10);
+            novoSalario.innerText = aumentoSalario;
+            console.log(aumentoSalario);
+            break;
+        case 'B':
+            aumentoSalario = salarioFormatado + (salarioFormatado * 15);
+            novoSalario.innerText = aumentoSalario;
+            console.log(aumentoSalario);
+            break;
+        case 'C':
+            aumentoSalario = salarioFormatado + (salarioFormatado * 20);
+            novoSalario.innerText = aumentoSalario;
+            console.log(aumentoSalario);
+            break;
+    }
+};
+
+
+
